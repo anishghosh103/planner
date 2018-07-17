885e1a543913ddde330921cc1a51f0aa3f60b9c4
Anish Ghosh <anishghosh103@gmail.com>
2018-07-17 08:52:12 +0530

    Update server:baseUrl for deployment

--- server/config/index.js
+++ server/config/index.js
@@ -3,8 +3,7 @@ const config = {};
 config.port = 3003;
 config.secret = 'asdlkj123u890asd01u9n20ausd';
 config.dataKey = 'eyJhbGciOiJIUzI';
-// TODO: change the 'baseUrl'
-config.baseUrl = `http://localhost:${config.port}`;
+config.baseUrl = `http://planner.anishghosh.me`;
 
 config.db = {
   uri: 'mongodb://127.0.0.1:27017/planner'
